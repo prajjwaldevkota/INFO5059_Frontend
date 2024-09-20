c2e35ea87e7490d8490c2f3b7d13bfffe69721a9
prajjwaldevkota <68115199+prajjwaldevkota@users.noreply.github.com>
2024-09-20 14:08:46 -0400

    Fixed Indentation

--- cypress/e2e/Vendor/vendor-delete.spec.cy.ts
+++ cypress/e2e/Vendor/vendor-delete.spec.cy.ts
@@ -1,10 +1,10 @@
 describe('vendor delete test', () => {
   it('visits the vendor page and deletes an vendor', () => {
-  cy.visit('/');
-  cy.get('button').click();
-  cy.contains('a', 'vendor').click();
-  cy.contains('Vendor').click();
-  cy.get('button').contains('Delete').click();
-  cy.contains('deleted!');
+    cy.visit('/');
+    cy.get('button').click();
+    cy.contains('a', 'vendor').click();
+    cy.contains('Vendor').click();
+    cy.get('button').contains('Delete').click();
+    cy.contains('deleted!');
   });
-  });
+});
--- cypress/e2e/Vendor/vendor.spec.cy.ts
+++ cypress/e2e/Vendor/vendor.spec.cy.ts
@@ -1,8 +1,8 @@
 describe('Vendor page test', () => {
   it('Visits the Vendor project page', () => {
-  cy.visit('/');
-  cy.get('button').click();
-  cy.contains('a', 'vendor').click();
-  cy.contains('Vendors loaded!!');
+    cy.visit('/');
+    cy.get('button').click();
+    cy.contains('a', 'vendor').click();
+    cy.contains('Vendors loaded!!');
   });
-  });
+});
